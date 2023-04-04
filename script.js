74208113e2c6a7fa4e3b3990eead49646ca9761c
Enrico Restuccia <enrico.rest98@gmail.com>
2023-04-04 15:11:03 +0200

    Funzionamento del quiz: Selezionare una risposta

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-/* TODO: inserite il codice JavaScript necessario a completare il MHW! */
+function uncheckAnswer(answer) {
+  answer.querySelector(".checkbox").src = unchecked;
+  answer.classList.remove("active");
+}
+
+function checkAnswer(answer) {
+  answer.querySelector(".checkbox").src = checked;
+  answer.classList.add("active");
+}
+
+function clickHandle(event) {
+  const { currentTarget } = event;
+
+  const otherAnswers = document.querySelectorAll(
+    `.choice-grid>div[data-question-id=${currentTarget.dataset.questionId}]`
+  );
+
+  // Uncheck all
+  otherAnswers.forEach((answer) => {
+    uncheckAnswer(answer);
+  });
+  console.log(otherAnswers);
+
+  // Check Clicked
+  checkAnswer(currentTarget);
+}
+
+// ----
+// MAIN
+// ----
+
+const checked = "images/checked.png";
+const unchecked = "images/unchecked.png";
+
+const answers = document.querySelectorAll(".choice-grid>div[data-question-id]");
+
+answers.forEach((el) => {
+  el.addEventListener("click", (event) => clickHandle(event));
+});
